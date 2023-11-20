37c51b1075651f9a46fde7acd9a7ec47c571629b
kalys <jsvsjsvshsksb@gmail.com>
2023-11-20 13:09:42 +0600

    fix_main_system

--- mood.py
+++ mood.py
@@ -1,5 +1,6 @@
 import random
 from better_profanity import profanity
+from datetime import datetime, timedelta
 
 def talk(person, text):
     RED   = "\033[1;31m"  
@@ -27,148 +28,205 @@ def talk(person, text):
         print(BLUE + "                 |  ~~~~~  |           " + RESET)
         print(BLUE + "                 |_________|           " + RESET)
 
+def time():
+    hour = (datetime.utcnow() + timedelta(hours=12)).hour
+    if hour >= 6 and hour <= 12:
+        return "Guten morgen"
+    elif hour >= 13 and hour <= 18:
+        return "Guten tag"
+    elif hour >= 19 and hour <= 24:
+        return "Guten abend"
+    elif hour >= 1 and hour <= 5:
+        return "Guten nacht"
+
+
 good = [
-    "good", "hey", "hi", "nice", "cool", "hello", "positive", "vibrant",
-    "excellent", "awesome", "fantastic", "superb", "amazing", "great", "wonderful", "happy",
-    "cheerful", "friendly", "joyful", "upbeat", "splendid", "terrific", "fabulous"
+    "Feeling good",
+    "Having a nice day",
+    "Feeling happy",
+    "Today is great",
+    "Hi! Feeling awesome",
+    "Feeling fantastic",
+    "Everything's wonderful",
+    "Great mood",
+    "Absolutely amazing",
+    "Hello, superb feeling",
+    "Feeling incredible",
+    "Terrific day",
+    "Superb",
+    "Excellent mood",
+    "Awesome",
+    "Marvelous",
+    "Brilliant",
+    "Splendid",
+    "Cheerful and positive",
+    "Joyful vibes",
 ]
 
 bad = [
-    "meh", "bad", "hard", "tough", "blah", "ugh", "down", "not good", "worse", "struggling",
-    "nope", "setbacks", "difficult", "challenging", "frustrating", "unpleasant", "discouraging",
-    "grim", "harsh", "disheartening", "painful", "uncomfortable"
+    "Not so good",
+    "Tough day",
+    "Feeling down",
+    "Struggling a bit",
+    "Not great today",
+    "Challenging times",
+    "Setbacks today",
+    "Bit frustrating",
+    "A bit harsh",
+    "Feeling discouraged",
+    "Facing difficulties",
+    "Unpleasant day",
+    "Discouraging moments",
+    "Feeling grim",
+    "Harsh reality",
+    "Disheartening",
+    "Uncomfortable",
+    "Meh",
+    "Blah",
 ]
 
 normal = [
-    "okay", "normal", "usual", "fine", "typical", "standard", "ordinary", "routine", "plain",
-    "same", "consistent", "steady", "average", "common", "balanced", "moderate", "routine",
-    "unchanged", "so-so", "mediocre"
+    "Just a day",
+    "Same old",
+    "Usual stuff",
+    "It's okay",
+    "Hey, normal day",
+    "Routine stuff",
+    "Typical day",
+    "Steady as usual",
+    "Balanced day",
+    "Maintaining routine",
+    "Common day",
+    "Consistent",
+    "Unchanged",
+    "So-so",
+    "Average day",
+    "Ordinary routine",
+    "Nothing special",
+    "Moderate day",
+    "Fair enough",
+    "Acceptable",
+] 
+
+positive_responses = [
+    "Glad to hear about your positive day",
+    "That's fantastic news, thanks for sharing",
+    "Awesome! Your happiness is contagious",
+    "Great to know you're feeling good",
+    "Happy for you and your positive vibes",
+    "Excellent! Keep that positivity going",
+    "Fantastic! What a wonderful day for you",
+    "Wonderful news! Your positivity is uplifting",
+    "Terrific! Your good mood brightened my day",
+    "Brilliant! Wishing you more moments like this",
+    "Superb! Your positivity is truly inspiring",
+    "Incredible! Your good mood is infectious",
+    "Fantastic news! Thanks for sharing your joy",
+    "Amazing! Your positive energy is palpable",
+    "Marvelous! Keep embracing the good vibes",
+    "Impressive! Your positivity is shining through",
+    "Splendid! Your happiness is well-deserved",
+    "Cheering for you and your continued joy",
+    "Keep it up! Your positive outlook is wonderful",
+    "Bravo! Your good day is well-celebrated"
 ]
 
-upset = [
-    "upset", "nerves", "irritated", "annoyed", "stressed", "frustrated", "challenged",
-    "distressed", "displeased", "worried", "angry", "unhappy", "disheartened", "discontent",
-    "disturbed", "perturbed", "miffed", "agitated", "irked", "enraged", "exasperated",
-    "infuriated", "fuming", "livid", "outraged"
+negative_responses = [
+    "I'm sorry to hear that you're not feeling great",
+    "That sounds tough, but you're not alone",
+    "Hang in there. Tough times don't last forever",
+    "Things will get better, keep your head up",
+    "Sending positive vibes your way",
+    "Sorry to hear that you're facing challenges",
+    "Stay strong. You've overcome hurdles before",
+    "Wishing you strength during difficult times",
+    "Tomorrow is a new day with new possibilities",
+    "Here for you, offering support and empathy",
+    "Take it one step at a time, progress is progress",
+    "Sending support and positive thoughts your way",
+    "You're not alone in facing life's difficulties",
+    "I hope things improve for you soon",
+    "Keep your head up, brighter days are ahead",
+    "It'll pass. Remember, you're resilient",
+    "Sending love and strength during tough times",
+    "Stay positive, you've got the strength within",
+    "You've got this. Lean on your support system",
+    "I'm here if you need to talk or vent"
+]
+
+neutral_responses = [
+    "Got it. Your update is noted",
+    "Thanks for sharing your thoughts",
+    "Noted. Anything else on your mind?",
+    "Alright. Let me know if you need anything",
+    "Understood. Feel free to share more",
+    "Cool. Anything specific you'd like to discuss?",
+    "Okay. If there's anything you'd like to talk about, I'm here",
+    "Fair enough. Your input is appreciated",
+    "No worries. Is there anything you'd like to add?",
+    "Sounds good. Let me know if you have more to share",
+    "Gotcha. If there's anything on your mind, I'm here",
+    "I see. Anything else you'd like to mention?",
+    "Sure thing. Is there anything specific on your mind?",
+    "Fine by me. Is there a particular topic you're interested in?",
+    "Alright then. Feel free to steer the conversation",
+    "Roger that. If there's anything pressing, let me know",
+    "All good. Is there something specific you'd like to discuss?",
+    "Sounds like a plan. Any particular direction you'd like to go?",
+    "Sure, why not. Is there a topic you'd like to explore?",
+    "Okay. If you have any questions or thoughts, feel free to share"
+]
+
+
+fallback_responses = [
+    "Hmm, that gives me something to ponder",
+    "Interesting, I'll keep that in mind",
+    "I see, your perspective is noted",
+    "Thanks for sharing, let's explore that further",
+    "Fascinating, I'll take that into consideration",
+    "Your input is intriguing, let's continue the conversation",
+    "I appreciate your insight, let's delve deeper",
+    "Noted, your thoughts are valued",
+    "Hmm, that's an interesting angle",
+    "Thanks for expressing that, let's move forward",
+    "I find your perspective thought-provoking",
+    "That's an interesting point, care to elaborate?",
+    "Interesting take! How do you see that impacting things?",
+    "Your thoughts add an interesting layer to the discussion",
+    "I'm intrigued! Can you share more about your viewpoint?",
+    "Your input is valued. What other thoughts do you have?",
+    "I'm curious to learn more about your perspective",
+    "Thanks for contributing. Anything else on your mind?",
+    "Your perspective is unique. What led you to that conclusion?",
+    "Let's explore this further. What brought you to this viewpoint?",
 ]
 
 
 
 
-good_responses = [
-    "I'm here for you, sending positive vibes your way. Let's focus on the good moments today.",
-    "Hey! I'm here to brighten your day. Share with me the positive things happening around you.",
-    "Hello! We can navigate through any challenges together. What's going well for you?",
-    "Sending you a virtual hug and lots of positive energy. You've got this!",
-    "Your resilience is inspiring! Let's celebrate the victories, no matter how small.",
-    "I'm grateful to be part of your support system. What positive experiences can we highlight today?",
-]
-
-bad_responses = [
-    "Hey, I'm here to support you during this rough patch. Let's work through it together.",
-    "Hi, I understand it's been a challenging day. You're not alone; I'm here for you.",
-    "Dealing with unexpected hurdles is tough, but I'm here to offer support and encouragement.",
-    "Even on tough days, your strength shines through. How can I assist you right now?",
-    "Your ability to face challenges head-on is commendable. Let's talk about what's on your mind.",
-    "Remember, it's okay not to be okay. I'm here to listen without judgment.",
-]
-
-normal_responses = [
-    "Just dropping by to say hello! If there's anything on your mind, feel free to share. I'm here for you.",
-    "Hey! Today is quite ordinary. How are you feeling? Anything you want to talk about?",
-    "Hello! If there's anything you need or want to discuss, I'm here to listen.",
-    "Wishing you a day filled with moments of joy and positivity. What's on your agenda?",
-    "Sometimes the simplest moments can bring the most joy. What's a small thing that made you smile today?",
-    "Life's full of surprises. Is there anything you're looking forward to in the coming days?",
-]
-
-upset_responses = [
-    "Hey, thanks for sharing. I'm here for you, and we can work through these emotions together.",
-    "I appreciate you opening up. Let's take it one step at a time, and I'm here to support you.",
-    "Hi, I'm sorry to hear that. Your feelings are valid, and I'm here to lend a listening ear.",
-    "Expressing your emotions is a sign of strength. How can we make the path ahead a bit lighter for you?",
-    "It's okay not to have all the answers. Together, we can explore ways to navigate these challenging emotions.",
-    "You're not alone in this. Let's acknowledge the difficulties and find ways to move forward.",
-]
-
-neutral = [
-    "I might need a bit more clarity. Could you please expand on that point, and we can work through it together?",
-    "I'm not entirely sure I understood. Can you provide more details, so we can go through it step by step?",
-    "It seems I may have missed something. Could you share more about what you meant? I'm here to help.",
-    "I want to make sure I understand correctly. Could you clarify or provide more context? We can tackle it together.",
-    "Sometimes, the details make all the difference. What specific aspects would you like to delve deeper into?",
-    "Your perspective is valuable. Let's dig into the details to gain a clearer understanding together.",
-]
-
-
-
-# amount of words
-def has_only_one_word(s):
-    if s.count(" ") == 0:
-        return True
-    return False
 
 # identifying the biggest amount of characters
 def robot_responce(mood):
-    most = ""
-    num = 0
-    for k,v in mood.items():
-        if v > num:
-            num = v
-            most = k
-    if most == "good":
-        subject = good_responses
-    elif most == "bad":
-        subject = bad_responses
-    elif most == "normal":
-        subject = normal_responses
-    elif most == "upset":
-        subject = upset_responses
-    else:
-        subject = neutral
-    response = random.randint(1, len(subject))
-    talk("robot", subject[response-1])
-    return subject[response-1]
+    response = random.randint(1, len(mood))
+    talk("robot", mood[response-1])
+    return mood[response-1]
 
 answers = []
-mood = {
-    "good":0,
-    "bad":0,
-    "normal":0,
-    "upset":0,
-    "not_id":0
-}
-talk("robot", "Hello my friend!")
-a = 1 # loop part
+talk("robot", f"{time} my friend!")
+a = 1
 while a <= 3:
     print('\n')
     me = input("Your answer to my greeting: ")
     talk("human", me)
     me = me.lower().replace('!', '')
-    if has_only_one_word(me):
-        if me in good:
-            mood["good"] += 1
-        elif me in bad:
-            mood["bad"] += 1
-        elif me in normal:
-            mood["normal"] += 1
-        elif me in upset:
-            mood["upset"] += 1
-        else:
-            mood["not_id"] += 1
+    if me in good:
+        mood = positive_responses
+    elif me in bad:
+        mood = negative_responses
+    elif me in normal:
+        mood = neutral_responses
     else:
-        mee = me.split(' ')
-        for word in mee:
-            if word in good:
-                mood["good"] += 1
-            elif word in bad:
-                mood["bad"] += 1
-            elif word in normal:
-                mood["normal"] += 1
-            elif word in upset:
-                mood["upset"] += 1
-            else:
-                mood["not_id"] += 1
+        mood = fallback_responses
     answers.append({me:robot_responce(mood)})
     a += 1
 
